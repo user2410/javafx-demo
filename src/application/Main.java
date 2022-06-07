f5b95a6da709d02d7f38a3a866ea8d02a9b98d70
user2410 <dc241024@gmail.com>
2022-06-08 00:15:07 +0700

    7.GridPane + textinput

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 
 
@@ -20,14 +24,32 @@ public class Main extends Application {
 				closeProgram();
 			});
 			
-			Button b1 = new Button("Close the program");
+			GridPane grid = new GridPane();
+			grid.setPadding(new Insets(10, 10, 10, 10));
+			grid.setVgap(8);
+			grid.setHgap(10);
 			
-			b1.setOnAction(e -> closeProgram());
+			Label nameLabel = new Label("Username: ");
+			GridPane.setConstraints(nameLabel, 0, 0); // set position of node
+			
+			// TextField nameInput = new TextField("Name"); // default text
+			TextField nameInput = new TextField();
+			nameInput.setPromptText("username");
+			GridPane.setConstraints(nameInput, 1, 0);
 
-			StackPane layout = new StackPane();
-			layout.getChildren().add(b1);
+			Label passLabel = new Label("Password: ");
+			GridPane.setConstraints(passLabel, 0, 1); // set position of node
 			
-			Scene scene = new Scene(layout, 600, 400);
+			TextField passInput = new TextField();
+			passInput.setPromptText("password");
+			GridPane.setConstraints(passInput, 1, 1);
+			
+			Button loginBtn = new Button("Login");
+			GridPane.setConstraints(loginBtn, 1, 2);
+			
+			grid.getChildren().addAll(nameLabel, nameInput, passLabel, passInput, loginBtn);
+			
+			Scene scene = new Scene(grid, 600, 400);
 			window.setScene(scene);
 			window.show();
 		} catch(Exception e) {
