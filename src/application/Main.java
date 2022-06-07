8fe53f5ee41f333ad2ca0271107ac1a48fff2c73
user2410 <dc241024@gmail.com>
2022-06-07 17:15:40 +0700

    initial commit

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,37 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.stage.Stage;
 import javafx.scene.layout.StackPane;
 import javafx.scene.Scene;
 
-import javafx.scene.control.Button;
-import javafx.scene.shape.Rectangle;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Button b1 = new Button("Button 1");
-			b1.setOnAction(new EventHandler<ActionEvent>() {
-
-				@Override
-				public void handle(ActionEvent arg0) {
-					// TODO Auto-generated method stub
-					System.out.println("Button 1 clicked");
-				}
-				
-			});
-			
-			// Rectangle rect = new Rectangle(5.0, 10.0, 100.0, 100.0);
-			
 			StackPane root = new StackPane();
-			root.getChildren().add(b1);
-			// root.getChildren().add(rect);
 			
-			Scene scene = new Scene(root, 600, 400);
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.setTitle("Javafx demo");
 			primaryStage.show();
