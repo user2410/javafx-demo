65039c46cb20aca6bc3e9b2caa30541e3ad2864a
user2410 <dc241024@gmail.com>
2022-06-09 12:33:00 +0700

    8.extract and validate user's input

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TextField;
+// import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -20,18 +23,35 @@ public class Main extends Application {
 				closeProgram();
 			});
 			
-			Button b1 = new Button("Close the program");
-			
-			b1.setOnAction(e -> closeProgram());
+			// Form
+			TextField inputField = new TextField();
+			Button submitBtn = new Button("Submit");
+			submitBtn.setOnAction(e -> {
+				checkUserInput(inputField);
+				System.out.println(inputField.getText());
+			});
 
-			StackPane layout = new StackPane();
-			layout.getChildren().add(b1);
+			VBox layout = new VBox(10);
+			layout.setPadding(new Insets(10, 10, 10, 10));
+			layout.getChildren().addAll(inputField, submitBtn);
 			
 			Scene scene = new Scene(layout, 600, 400);
 			window.setScene(scene);
 			window.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private boolean checkUserInput(TextField inputField) {
+		String msg = inputField.getText();
+		try {
+			int num = Integer.parseInt(msg);
+			System.out.println(num);
+			return true;
+		}catch(NumberFormatException e) {
+			System.err.println(msg + "is not a number");
+			return false;
 		}
 	}
 	
