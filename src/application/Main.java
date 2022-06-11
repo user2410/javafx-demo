17c0a34bab3a5375d12346cb81cae3e7732c622f
user2410 <dc241024@gmail.com>
2022-06-11 09:33:20 +0700

    13.TreeView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -19,13 +22,32 @@ public class Main extends Application {
 				e.consume();
 				closeProgram();
 			});
-			
-			Button b1 = new Button("Close the program");
-			
-			b1.setOnAction(e -> closeProgram());
 
-			StackPane layout = new StackPane();
-			layout.getChildren().add(b1);
+			
+			TreeItem<String> root, branch1, branch2;
+			
+			root = new TreeItem<>();
+			root.setExpanded(true);
+			
+			branch1 = makeBranch("b1", root);
+			makeBranch("b11", branch1);
+			makeBranch("b12", branch1);
+			makeBranch("b13", branch1);
+			
+			branch2 = makeBranch("b2", root);
+			makeBranch("b21", branch2);
+			makeBranch("b22", branch2);
+			
+			TreeView<String> tree = new TreeView<String>(root);
+			tree.setShowRoot(false);
+			tree.getSelectionModel().selectedItemProperty().addListener(
+					(v, oldVal, newVal)->System.out.println(newVal.getValue()));
+
+			Button b1 = new Button("Close the program");
+
+			VBox layout = new VBox(10);
+			layout.setPadding(new Insets(20, 20, 20, 20));
+			layout.getChildren().addAll(tree, b1);
 			
 			Scene scene = new Scene(layout, 600, 400);
 			window.setScene(scene);
@@ -33,6 +55,13 @@ public class Main extends Application {
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+	}
+	
+	private TreeItem<String> makeBranch(String name, TreeItem<String> parent){
+		TreeItem<String> item = new TreeItem<String>(name);
+		item.setExpanded(true);
+		parent.getChildren().add(item);
+		return item;
 	}
 	
 	private void closeProgram() {
