086ea2469541fcce3d47a0400068194ba8633b11
user2410 <dc241024@gmail.com>
2022-06-11 09:07:20 +0700

    12.ListView

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,14 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.control.ListView;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -20,18 +24,35 @@ public class Main extends Application {
 				closeProgram();
 			});
 			
-			Button b1 = new Button("Close the program");
+			ListView<String> list = new ListView<String>();
+			list.getItems().addAll(
+					"Iron man",
+					"Titanic",
+					"Contact",
+					"Surrogate"
+			);
+			list.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 			
-			b1.setOnAction(e -> closeProgram());
-
-			StackPane layout = new StackPane();
-			layout.getChildren().add(b1);
+			Button b1 = new Button("Click me");
+			b1.setOnAction(e->buttonClicked(list));
+			
+			VBox layout = new VBox(10);
+			layout.setPadding(new Insets(20, 20, 20, 20));
+			layout.getChildren().addAll(list, b1);
 			
 			Scene scene = new Scene(layout, 600, 400);
 			window.setScene(scene);
 			window.show();
 		} catch(Exception e) {
 			e.printStackTrace();
+		}
+	}
+	
+	private void buttonClicked(ListView<String> list) {
+		ObservableList<String> movies = list.getSelectionModel().getSelectedItems();
+		
+		for(String m : movies) {
+			System.out.println(m);
 		}
 	}
 	
