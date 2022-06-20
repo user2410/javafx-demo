06337b0853a8a36b339cf6e981c0ba767f9abc17
user2410 <dc241024@gmail.com>
2022-06-20 22:54:00 +0700

    simple binding

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 
 
 public class Main extends Application {
@@ -12,7 +17,23 @@ public class Main extends Application {
 	public void start(Stage primaryStage) {
 		try {
 			window = primaryStage;
-						
+			
+			// Text field
+			TextField userInput = new TextField();
+			userInput.setMaxWidth(200);
+			
+			Label firstLabel = new Label("Welcome");
+			Label secondLabel = new Label();
+			HBox bottomText = new HBox(firstLabel, secondLabel);
+			bottomText.setAlignment(Pos.CENTER);
+			secondLabel.textProperty().bind(userInput.textProperty());
+			
+			VBox vbox = new VBox(10, userInput, bottomText);
+			vbox.setAlignment(Pos.CENTER);
+			
+			Scene scene = new Scene(vbox, 300, 200);
+			
+			window.setScene(scene);
 			window.setTitle("JavaFx demo");
 			window.show();
 		} catch(Exception e) {
